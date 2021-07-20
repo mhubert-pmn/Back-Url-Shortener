a0fad4219d1756a2c7ed027b3f5261a62fb7b61c
CHRIS BAHONDA <gyde-feiry.chris-bahonda@capgemini.com>
2021-07-20 15:15:54 +0200

    Add user model

---
+++ src/models/userModel.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql');
+const { ObjectId } = mongoose.Schema;
+
+const userSchema = new mysql.Schema(
+    {
+        idUser:{
+            type: Number,
+            required: true,
+            maxlength: 50,
+            unique: true
+        },
+
+        pseudo:{
+            type: String,
+            required: true,
+            maxlength: 60
+        },
+
+        email:{
+            type: String,
+            required: true
+        },
+
+        password:{
+            type: String,
+            required: true
+        }
+
+    })
+
+module.exports = mysql.model('User', userSchema);
