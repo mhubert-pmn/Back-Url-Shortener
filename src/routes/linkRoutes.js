b1f270951b0f92355298b7323796858f761f0475
Matthieu <hubmattfac@gmail.com>
2021-07-21 17:02:47 +0200

    [Fix] Deleted unnecessary route

--- src/routes/linkRoutes.js
+++ src/routes/linkRoutes.js
@@ -4,7 +4,4 @@ module.exports = (server) => {
     server.route('/links')
         .get(linkController.findAll)
         .post(linkController.create);
-
-    server.route('/user/:id_user/links')
-        .get(linkController.getUserLinks);
 }
